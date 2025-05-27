2e7f27ce9e7a31514a4ba37fba6e3852c9da6145
Shimadakunn <89693356+Shimadakunn@users.noreply.github.com>
2025-05-27 18:13:11 +0200

    node

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { serve } from "@hono/node-server";
 import user from "./routes/user";
 import waitingList from "./routes/waiting-list";
 import moonpay from "./routes/moonpay";
@@ -9,7 +10,8 @@ app.route("/user", user);
 app.route("/moonpay", moonpay);
 app.route("/waiting-list", waitingList);
 
-Bun.serve({
-  port: process.env.PORT || 3000,
+const port = process.env.PORT || 3000;
+serve({
   fetch: app.fetch,
+  port: Number(port),
 });
